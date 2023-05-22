784608dc10e7a3b863f06e10ad1fde98a288ea3e
mahsunberkem34 <mahsunberkem@gmail.com>
2023-05-22 13:10:33 +0300

    baka

--- examp2/example2.js
+++ examp2/example2.js
@@ -6,5 +6,40 @@
 
 // her aday için bir değişken
 
-const aday1 = 0;
-const aday2 = 0;
+// Adayların başlangıç oy sayıları
+let erdoganVotes = 0;
+let kilicdarogluVotes = 0;
+let muharremVotes = 0;
+let sinanVotes = 0;
+
+// Erdoğan butonuna tıklandığında oy sayısını artırma
+const erdoganButton = document.querySelector(".btn-erdogan");
+const erdoganVotesDisplay = document.getElementById("erdoganVotes");
+erdoganButton.addEventListener("click", function () {
+  erdoganVotes++;
+  erdoganVotesDisplay.textContent = erdoganVotes;
+});
+
+// Kılıçdaroğlu butonuna tıklandığında oy sayısını artırma
+const kilicdarogluButton = document.querySelector(".aday2 .btn-oy");
+const kilicdarogluVotesDisplay = document.getElementById("kilicdarogluVotes");
+kilicdarogluButton.addEventListener("click", function () {
+  kilicdarogluVotes++;
+  kilicdarogluVotesDisplay.textContent = kilicdarogluVotes;
+});
+
+// Muharrem butonuna tıklandığında oy sayısını artırma
+const muharremButton = document.querySelector(".aday3 .btn-oy");
+const muharremVotesDisplay = document.getElementById("muharremVotes");
+muharremButton.addEventListener("click", function () {
+  muharremVotes++;
+  muharremVotesDisplay.textContent = muharremVotes;
+});
+
+// Sinan butonuna tıklandığında oy sayısını artırma
+const sinanButton = document.querySelector(".aday4 .btn-oy");
+const sinanVotesDisplay = document.getElementById("sinanVotes");
+sinanButton.addEventListener("click", function () {
+  sinanVotes++;
+  sinanVotesDisplay.textContent = sinanVotes;
+});
